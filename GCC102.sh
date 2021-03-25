94c754df7983f94e3d6737312e2bc8b951ed10e7
Daffa06 <Daffa@DaffaMX>
2021-03-25 20:00:08 +0700

    Fix send channel

--- GCC102.sh
+++ GCC102.sh
@@ -60,7 +60,7 @@ PTTG=1
 	if [ $PTTG = 1 ]
 	then
 		# Set Telegram Chat ID
-		CHATID="-1233987016"
+		CHATID="-1001233987016"
 	fi
 
 # Generate a full DEFCONFIG prior building. 1 is YES | 0 is NO(default)
